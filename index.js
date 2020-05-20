17cc72bea947377d1168b184c58b805e8d524110
Jordan Surgick <jordansurgick@aol.com>
2020-05-20 14:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Jordy Surgick';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
